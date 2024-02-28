018f7cbc23e3124b979858d46e41d6614b763785
Ivan Gorbunov <I.MGorbunov@yandex.ru>
2024-02-28 21:48:47 +0400

    first commit

---
+++ js/modal.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector(".search-model");
+const closeModalButton = modal.querySelector(".icon_close");
+const searchInput = modal.querySelector("#search-input");
+
+const openModalButton = document.querySelector(".icon_search");
+
+openModalButton.addEventListener("click", () => {
+  clearInput();
+  modal.style.display = "block";
+});
+
+closeModalButton.addEventListener("click", () => {
+  clearInput();
+  modal.style.display = "none";
+});
+
+searchInput.addEventListener("input", () => {
+  console.log(e.target.value);
+});
+
+const clearInput = () => {
+  searchInput.value = "";
+};
